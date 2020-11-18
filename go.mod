025977885ec4e19df2e6b26c434a4d83a1e3cb23
Rodrigo Silva Mendoza <rodrigo.silvamendoza2@aexp.com>
2020-11-18 09:14:49 -0700

    feat(libvips): use libvips

--- go.mod
+++ go.mod
@@ -5,5 +5,5 @@ go 1.13
 require (
 	github.com/aws/aws-lambda-go v1.20.0
 	github.com/aws/aws-sdk-go v1.35.29
-	github.com/disintegration/imaging v1.6.2
+	github.com/h2non/bimg v1.1.4
 )
--- main.go
+++ main.go
@@ -4,21 +4,20 @@ import (
 	//"context"
 	"bytes"
 	"context"
-	"image"
-	"image/jpeg"
 	"log"
 	"os"
 	"path/filepath"
 	"strings"
 	"sync"
+	"time"
 
 	"github.com/aws/aws-lambda-go/events"
-	"github.com/aws/aws-lambda-go/lambda"
+	//"github.com/aws/aws-lambda-go/lambda"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/aws/aws-sdk-go/service/s3/s3manager"
-	"github.com/disintegration/imaging"
+	"github.com/h2non/bimg"
 )
 
 type PhotoSize struct {
@@ -79,13 +78,12 @@ func HandleRequest(ctx context.Context, event events.S3Event) (string, error) {
 			return "", err
 		}
 
-		img, err := decodeImageFromBytes(&photoBuff, fileWithoutExtension)
-
 		if err != nil {
 			log.Printf("Could not decode downloaded image %s", srcKey)
 			return "", err
 		}
 
+		photoBytes := photoBuff.Bytes()
 		s3uploader := s3manager.NewUploader(s3Ses)
 		var wg sync.WaitGroup
 		wg.Add(len(defaultPhotoSizes))
@@ -95,7 +93,10 @@ func HandleRequest(ctx context.Context, event events.S3Event) (string, error) {
 			go func() {
 				log.Printf("size in go func is %v", size)
 				defer wg.Done()
-				resizedImage := resizeImage(&img, &size)
+				resizedImage, err := resizeImage(&photoBytes, &size)
+				if err != nil {
+					return
+				}
 				encodeImageAndUploadToS3(resizedImage, fileWithoutExtension, fileExt, size.Suffix, s3uploader)
 			}()
 		}
@@ -105,43 +106,32 @@ func HandleRequest(ctx context.Context, event events.S3Event) (string, error) {
 	return "Succesfully processed req", nil
 }
 
+func timeTrack(start time.Time, name string) {
+	elapsed := time.Since(start)
+	log.Printf("function %s took %s", name, elapsed)
+}
+
 func getImageNameAndExt(imgPath string) (string, string) {
 	fileExt := filepath.Ext(imgPath)
 	fileWithoutExtension := strings.TrimSuffix(imgPath, fileExt)
 	return fileWithoutExtension, fileExt
 }
 
-func decodeImageFromBytes(photoBuff *aws.WriteAtBuffer, imageName string) (image.Image, error) {
-	// Get the bytes from the buffer and decode into image
-	imageBytes := photoBuff.Bytes()
-	reader := bytes.NewReader(imageBytes)
-	decodedImg, err := jpeg.Decode(reader)
+func resizeImage(imgBytes *[]byte, requestedSize *PhotoSize) ([]byte, error) {
+	newImg, err := bimg.NewImage(*imgBytes).Resize(requestedSize.Width, 0)
 
 	if err != nil {
-		log.Printf("Could not decode downloaded image %s", imageName)
+		log.Printf("Could not create image from byte[]")
 		return nil, err
 	}
-
-	return decodedImg, nil
+	return newImg, nil
 }
 
-func resizeImage(img *image.Image, requestedSize *PhotoSize) *image.NRGBA {
-	return imaging.Resize(*img, requestedSize.Width, 0, imaging.Lanczos)
-}
-
-func encodeImageAndUploadToS3(img *image.NRGBA, imgName string, imgExt string, newSizeSuffix string, s3uploader *s3manager.Uploader) {
+func encodeImageAndUploadToS3(img []byte, imgName string, imgExt string, newSizeSuffix string, s3uploader *s3manager.Uploader) {
 	s3Key := imgName + newSizeSuffix + imgExt
 
-	newBuf := new(bytes.Buffer)
-	err := jpeg.Encode(newBuf, img, nil)
-
-	if err != nil {
-		log.Printf("Error encoding image to buffer: %v\n", err)
-		return
-	}
-
 	res, err := s3uploader.Upload(&s3manager.UploadInput{
-		Body:        bytes.NewReader(newBuf.Bytes()),
+		Body:        bytes.NewReader(img),
 		Bucket:      aws.String(os.Getenv("RESIZED_PHOTOS_BUCKET")),
 		Key:         &s3Key,
 		ContentType: aws.String("image/jpeg"),
@@ -155,6 +145,35 @@ func encodeImageAndUploadToS3(img *image.NRGBA, imgName string, imgExt string, n
 	log.Printf("Uploaded %s successfully to %s\n", s3Key, res.Location)
 }
 
+func testLocaly() {
+	// Open a file in this directory
+	defer timeTrack(time.Now(), "testLocally")
+	buffer, err := bimg.Read("./snow_mountain_brandur.jpg")
+	if err != nil {
+		log.Printf("%v", err)
+	}
+
+	var wg sync.WaitGroup
+	wg.Add(len(defaultPhotoSizes))
+
+	for _, size := range defaultPhotoSizes {
+		size := size
+		go func() {
+			//defer timeTrack(time.Now(), "go("+size.Suffix+")")
+			log.Printf("size in go func is %v", size)
+			defer wg.Done()
+			resizedImage, err := resizeImage(&buffer, &size)
+			if err != nil {
+				return
+			}
+			bimg.Write("snow_mountain_brandur"+size.Suffix+".jpg", resizedImage)
+		}()
+	}
+	wg.Wait()
+	log.Printf("Resized all images for snow")
+}
+
 func main() {
-	lambda.Start(HandleRequest)
+	//lambda.Start(HandleRequest)
+	testLocaly()
 }
